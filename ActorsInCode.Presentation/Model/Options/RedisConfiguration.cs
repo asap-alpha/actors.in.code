cc9fa4cd582a5f026f11486c3b1907dde5610f65
Abdallah Mustapha <abdallah.mustapha@hubtel.com>
2024-12-01 23:20:21 +0000

    closing task on persisting sample payload to redis

---
+++ ActorsInCode.Presentation/Model/Options/RedisConfiguration.cs
@@ -0,0 +1,9 @@
+namespace ActorsInCode.Presentation.Model.Options;
+
+public class RedisConfiguration
+{
+    public string? RedisInstance { get; set; }
+    public int RedisDb { get; set; }
+}
+
+ 
--- ActorsInCode.Presentation/Services/Repository/RedisRepository.cs
+++ ActorsInCode.Presentation/Services/Repository/RedisRepository.cs
@@ -1,41 +1,45 @@
+using ActorsInCode.Presentation.Model.Options;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using StackExchange.Redis;
 
 namespace ActorsInCode.Presentation.Services.Repository;
 
-public class RedisRepository:IRedisRepository
+public class RedisRepository : IRedisRepository
 {
     private readonly IDatabase _database;
-  
 
-    public RedisRepository()
+    public RedisRepository(IOptions<RedisConfiguration> redisConfiguration)
     {
+        var redisConfig = redisConfiguration.Value;
         var configurationOptions = new ConfigurationOptions
         {
-            EndPoints = { "127.0.0.1:6379" }
-            
+            EndPoints = { redisConfig!.RedisInstance }
         };
         var connect = ConnectionMultiplexer.Connect(configurationOptions);
 
-        _database = connect.GetDatabase(6);
-        
+        _database = connect.GetDatabase(redisConfig.RedisDb);
     }
 
     public async Task<bool> Add(List<WeatherForecast> payloads)
     {
-        var isSuccessful  = false;
+        var isSuccessful = false;
         foreach (var payload in payloads)
         {
             var key = $"weather:{payload.Summary}";
+            var keyAlreadyExist = await _database.KeyExistsAsync(key, CommandFlags.None);
+            if (!keyAlreadyExist)
+            {
+                var result = await _database.StringSetAsync(key, JsonConvert.SerializeObject(payload),
+                    TimeSpan.FromMinutes(30));
 
-           var result   = await _database.StringSetAsync( key, JsonConvert.SerializeObject(payload), TimeSpan.FromMinutes(30));
-
-           if (result)
-           {
-               isSuccessful = true;
-           }
+                if (result)
+                {
+                    isSuccessful = true;
+                }
+            }
         }
-        
+
         return isSuccessful;
     }
 }
